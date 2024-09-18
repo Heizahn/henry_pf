b1f9bdc6b3c09e64c75f9151f61fb13172b3caf5
Heizahn <hrbweibezahn@gmail.com>
2024-09-17 23:41:21 -0400

    animacion en la landing

--- front/src/components/sections-home/sectionOne.tsx
+++ front/src/components/sections-home/sectionOne.tsx
@@ -1,3 +1,25 @@
-export default function SectionOne() {
-	return <section className='h-[90vh]'>Section 1</section>;
+'use client';
+
+import { useTransform, motion } from 'framer-motion';
+
+export default function SectionOne({ scrollYProgress }) {
+	const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+	const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
+
+	return (
+		<motion.div
+			style={{ scale, rotate }}
+			className='sticky top-[10vh] h-[90vh] bg-[#C72626] text-[3.5vw] flex flex-col items-center justify-center text-white -z-10'
+		>
+			<p>Scroll Perspective</p>
+
+			<div className='flex gap-4'>
+				<p>Section</p>
+
+				<div className='relative w-[12.5vw]'></div>
+
+				<p>Transition</p>
+			</div>
+		</motion.div>
+	);
 }
--- front/src/components/sections-home/sections.tsx
+++ front/src/components/sections-home/sections.tsx
@@ -1,11 +1,32 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
 import SectionOne from './sectionOne';
 import SectionTwo from './sectionTwo';
+import { useScroll } from 'framer-motion';
+import Lenis from 'lenis';
 
-export default function SectionsHome() {
+export default function Sections() {
+	const containerRef = useRef();
+	const { scrollYProgress } = useScroll({
+		target: containerRef.current,
+		offset: ['start start', 'end end'],
+	});
+
+	useEffect(() => {
+		const lenis = new Lenis();
+
+		function raf(time) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	}, []);
 	return (
-		<div>
-			<SectionOne />
-			<SectionTwo />
-		</div>
+		<main ref={containerRef} className='relative h-[180vh]'>
+			<SectionOne scrollYProgress={scrollYProgress} />
+			<SectionTwo scrollYProgress={scrollYProgress} />
+		</main>
 	);
 }
