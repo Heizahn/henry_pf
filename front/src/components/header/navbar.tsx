ad4d674ab69dd49154b86e0d0ecdb3558810a07f
Heizahn <humbertorbw@gmail.com>
2024-10-04 15:59:57 -0400

    quitando arrobas del navbar

--- front/src/components/header/navbar.tsx
+++ front/src/components/header/navbar.tsx
@@ -3,11 +3,11 @@
 import { ILinkNav } from '@/interfaces/interfaces';
 import LinkNav from './link-nav';
 // import HomeIcon from '/public/assets/gite.svg';
-import LibraryIcon from '@/public/assets/shelves.svg';
+import LibraryIcon from '/public/assets/shelves.svg';
 // import CategoryIcon from '/public/assets/category.svg';
-import CommunityIcon from '@/public/assets/groups.svg';
-import ProfileIcon from '@/public/assets/login.svg';
-import { useIsLoading } from '@/store/useUserStore';
+import CommunityIcon from '/public/assets/groups.svg';
+import ProfileIcon from '/public/assets/login.svg';
+import { useIsLoading } from '/store/useUserStore';
 
 export default function Navbar() {
 	const { isLoading } = useIsLoading();
